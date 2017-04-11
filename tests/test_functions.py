4b9a984306fe0cac872567d87cf9927c78e914de
Will Drevo <will.drevo@gmail.com>
2017-04-11 11:04:44 -0700

    Generator func for those on git

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,11 +2,28 @@ import unittest
 import tests
 
 if not tests.USE_ANSWERS:
-    from excercises.functions import power_list_factory, dict_updater_factory
+    from excercises.functions import power_list_factory, dict_updater_factory, \
+        cycle_colors
 else:
-    from answers.functions import power_list_factory, dict_updater_factory
+    from answers.functions import power_list_factory, dict_updater_factory, \
+        cycle_colors
 
 class FunctionExcercises(unittest.TestCase):
+
+    def test_cycle_colors(self):
+        def g(): yield None 
+        generator_type = type(g())
+        colors = ['r', 'g', 'b']
+
+        self.assertEqual(type(cycle_colors(colors)), generator_type)
+
+        color_generator = cycle_colors(colors)
+
+        for i in range(100000):
+            idx = i % len(colors)
+            color = next(color_generator)
+            self.assertEqual(colors[idx], color)
+
     def test_power_list_factory(self):
         func = power_list_factory()
         p = 2
